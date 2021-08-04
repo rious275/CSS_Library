4b845c637d0f4b2fa970e0d925cd5d042702796e
rious275 <rious275@naver.com>
2021-08-04 19:46:15 +0900

    [Add] 최초 push 및 모달창 작업 완료

---
+++ src/Modal/Modal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Modal(props) {
+  return (
+    <ModalContainer>
+      <ModalBg onClick={props.closeModal} />
+      <ModalBox>
+        <h3>Modal Test</h3>
+        <p>
+          Lorem ipsum dolor sit amet, <br />
+          consectetur adipiscing elit.
+        </p>
+      </ModalBox>
+    </ModalContainer>
+  );
+}
+
+export default Modal;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  height: 100%;
+`;
+
+const ModalBg = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalBox = styled.div`
+  position: fixed;
+  margin: 200px auto;
+  left: 0;
+  right: 0;
+  width: 340px;
+  text-align: center;
+  border-radius: 20px;
+  background-color: #fff;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+
+  h3 {
+    margin: 40px;
+    font-size: 30px;
+    font-weight: 600;
+  }
+
+  p {
+    margin: 40px;
+    font-size: 20px;
+  }
+`;
