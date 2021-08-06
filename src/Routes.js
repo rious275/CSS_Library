9480d40e02faa0cff9675e40d0a4e32eba59bd99
rious275 <rious275@naver.com>
2021-08-06 11:53:34 +0900

    slider01 추가

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ModalBtn from './Modal01/ModalBtn';
+import SliderSlick from './Slider01/SliderSlick';
 
 function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/Modal01" component={ModalBtn} />
+        <Route exact path="/modal01" component={ModalBtn} />
+        <Route exact path="/slider01" component={SliderSlick} />
       </Switch>
     </Router>
   );
